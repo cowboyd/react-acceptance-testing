b6ab62980136ffd302f2da66abb2f3a5c435c7b3
Charles Lowell <cowboyd@frontside.io>
2017-05-05 17:13:31 -0500

    load app in bundle, load tests in bundle

---
+++ tests/hello-test.js
@@ -0,0 +1,11 @@
+// import { describe, beforeEach, it } from 'mocha';
+import { App } from '../src/app';
+
+describe('Something Wonderful', function() {
+  beforeEach(function() {
+    this.foo = 'bar';
+  });
+  it('is truly wonderful', function() {
+    // expect(this.foo).to.equal('bar');
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    app: './src/index.js',
+    test: './tests/index.js'
+  },
 
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/public'
   },
