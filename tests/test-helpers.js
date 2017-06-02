2dc5f9e786cf14640bbf3f40edc6519a6075ea7f
Wil Wilsman <wil@frontside.io>
2017-06-02 09:38:56 -0500

    Clean up test helpers and create acceptance setup helper

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -3,10 +3,37 @@ import sinonChai from 'sinon-chai';
 import jqueryChai from 'chai-jquery';
 import chai from 'chai';
 
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+
+// set up our assertion library
 chai.use(sinonChai);
 chai.use((chai, utils) => jqueryChai(chai, utils, jQuery));
 
-export { default as $ } from 'jquery';
+// allows us to import jQuery from test-helpers
+export const $ = jQuery;
+
+// set up acceptance testing for an app
+export function setupAcceptanceTestingForApp(App) {
+  let container;
+
+  beforeEach(function() {
+    // set up our container where we mount our app
+    container = document.createElement('div');
+    container.id = '#testing';
+    document.body.appendChild(container);
+
+    // mount the app with props.test === true
+    this.app = render(<App test/>, container);
+  });
+
+  afterEach(function() {
+    // unmount the app and destroy our container
+    unmountComponentAtNode(container);
+    document.body.removeChild(container);
+    container = null;
+  });
+}
 
 // helper to loop over assertions until the test timeout
 export function assertUntilTimeout(fn) {
