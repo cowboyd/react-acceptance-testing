7518f905ccdda38c5b37037484d2febf87ca6504
Wil Wilsman <wil@frontside.io>
2017-06-02 10:18:41 -0500

    Use pretender to mock requests

---
+++ tests/fixtures.js
@@ -0,0 +1,42 @@
+export const GIF_FIXTURES = {
+  '1': {
+    id: '1',
+    images: {
+      original: {
+        width: 100,
+        height: 100,
+        url: 'http://placehold.it/100'
+      }
+    }
+  },
+  '2': {
+    id: '2',
+    images: {
+      original: {
+        width: 100,
+        height: 100,
+        url: 'http://placehold.it/100'
+      }
+    }
+  },
+  '3': {
+    id: '3',
+    images: {
+      original: {
+        width: 100,
+        height: 100,
+        url: 'http://placehold.it/100'
+      }
+    }
+  },
+  '4': {
+    id: '4',
+    images: {
+      original: {
+        width: 100,
+        height: 100,
+        url: 'http://placehold.it/100'
+      }
+    }
+  }
+};
